e3bee203f653efe71509e9281a2d233079c8eb05
Matthes Staacken <m.staacken@mbd-team.de>
2022-01-04 16:59:33 +0100

    test

--- src/views/DeckEditor.vue
+++ src/views/DeckEditor.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <div class="container mt-5">
+    <div class="card d-flex">
+      <div class="card-header">Decks</div>
+      <div class="card-body">
+        <div><button class="btn btn-secondary rounded-pill" type="button" @click="createDeck = true">create new Deck</button></div>
+        <div>
+          <form class="d-flex m-5">
+            <div>
+              <input type="text" placeholder="name" required />
+            </div>
+            <div></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,7 +22,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   data() {
-    return {};
+    return {
+      createDeck: false,
+    };
   },
 });
 </script>
+
+<style lang="scss"></style>
