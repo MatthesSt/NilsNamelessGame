86be8fcbfbe7568d7666c2df78a5f184730597d0
Newkiller <robocraft.tier5@outlook.de>
2022-01-14 22:45:17 +0100

    redesign Ingame vue

--- src/components/inGame.vue
+++ src/components/inGame.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="field" class="row">
-    <div id="player1" class="col">
-      <div id="tile"></div>
+  <div id="game" class="d-flex m-3 justify-content-center">
+    <div id="self" class="">
+      <div class="playerSection">
+        <div>{{ self.username }}</div>
+        <div class="tile m-2">deck: {{ self.deck }}</div>
+      </div>
     </div>
-    <div class="container col d-flex flex-column align-items-center">
-      <div v-for="row in 10" :key="row" class="row">
+    <div class="playerStrategy">
+      <div v-for="tile in 9" :key="tile">
+        <div class="tile">0,{{ tile }}</div>
+      </div>
+    </div>
+    <div id="field" class="d-flex flex-column align-items-center">
+      <div v-for="row in 9" :key="row" class="row">
         <div class="d-flex">
-          <div v-for="tile in 7" :key="tile" class="">
-            <div id="tile">{{ tile }},{{ row }}</div>
+          <div v-for="tile in 5" :key="tile" class="">
+            <div class="tile">{{ tile }},{{ row }}</div>
           </div>
         </div>
       </div>
     </div>
-    <div id="player2" class="col">abc</div>
+    <div class="playerStrategy">
+      <div v-for="tile in 9" :key="tile">
+        <div class="tile">6,{{ tile }}</div>
+      </div>
+    </div>
+    <div id="enemy" class="">
+      <div class="playerSection">
+        <div>{{ game.user2?.name ?? "enemy" }}</div>
+        <div class="tile m-2">deck</div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import * as API from "../../API";
+import { currentUser } from "@/router";
 
 export default defineComponent({
   props: {
@@ -26,23 +45,48 @@ export default defineComponent({
       required: true,
     },
   },
+  setup() {
+    return { currentUser };
+  },
   data() {
     return {
-      games: [] as API.game[],
+      game: {} as API.game,
+      enemy: {} as API.Player,
+      self: {} as API.Player,
       allowed: false,
     };
   },
   async mounted() {
-    this.games = await API.getGames();
-    console.log(this.games.find(game => (game.id = this.playing)));
+    this.game = (await API.getGames()).find((game: API.game) => (game.id = this.playing));
+    console.log(this.game);
+    switch (this.game.user1.id == this.currentUser?.uid) {
+      case true:
+        this.self = this.game.user1;
+        this.enemy = this.game.user2;
+        break;
+      case false:
+        this.enemy = this.game.user1;
+        this.self = this.game.user2;
+        break;
+    }
   },
 });
 </script>
 <style lang="scss">
-#tile {
+.tile {
   $size: 90px;
   border: 1px solid black;
   width: $size;
   height: $size;
+}
+.playerSection {
+  border: 2px solid black;
+  height: 100%;
+}
+.playerStrategy {
+  border: 1px solid green;
+}
+#field {
+  border: 1px solid blue;
 }
 </style>
